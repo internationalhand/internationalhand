745bdce757addd357f4a44ba34191126c7793e7a
Cyril Fluck <cfluck@VRs-MacBook-Pro-7.local>
2012-11-15 19:48:46 -0800

    Google Analytics integration

--- ihf.js
+++ ihf.js
@@ -45,3 +45,13 @@ ihf_load = function() {
 	navButton_init(4, "volunteer.html");
 	navButton_init(5, "donate.html");
 };
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-36374076-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
